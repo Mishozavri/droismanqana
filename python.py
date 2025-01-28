d18c23718781b791bef78efa97d6be46ce09f1f8
Mishozavri <mikheilabuashvili11@gmail.com>
2025-01-28 23:00:26 +0400

    დავლება 17
    დავალება 17

--- python.py
+++ python.py
@@ -1,33 +1,35 @@
-#1
-# მას კოდში არის რაღაცა არასწორი და არ პრინტავს ვერ მივხვდი რა მაქვს არადწორად გაკეთებული
-class Student:
+class Rectangle:
+    def __init__(self, width, length, color):
+        self.width = width
+        self.length = length
+        self.color = color
 
-    def __init__(self, first_name, last_name, id, birth_year, grades):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.id = id
-        self.birth_year = birth_year
-        self.grades = grades
+    def perimeter(self):
+        result = (self.width + self.length) + (self.width + self.length)
+        return result
+
+    def area(self):
+        result = 0
+        count = self.length
+        while count > 0:
+            result += self.width
+            count -= 1
 
     def __str__(self):
-        return f"name": {self.first_name}, last_name: {self.last_name}, id: {self.id}, birth_year: {self.birth_year}, grades: {self.grades}
-
-    def years_until_18(self):
-        current_year = 2025
-        age = current_year - self.birth_year
-        return max(0, 18 - age)
-
-student1 = Student("zezva", "pirveladze", "00000001", 2005, {"Math": 1, "Programming": -1000000})
-student2 = Student("mzia", "pirveladze", "00000001", 2017, {"Math": 2, "Programming": -10000000})
-student3 = Student("misho", "abuashvili", "000000002", 2011, {"Math": 100, "Programming": 60})
-
-print(student1)
-print(f"Years until 18: {student1.years_until_18()}\n")
-
-print(student2)
-print(f"Years until 18: {student2.years_until_18()}\n")
-
-print(student3)
-print(f"Years until 18: {student3.years_until_18()}\n")
+        return f"Rectangle(width={self.width}, length={self.length}, color={self.color})"
 
 
+# Creating objects
+obj1 = Rectangle(1, 5, "blue")
+obj2 = Rectangle(3, 3, "green")
+obj3 = Rectangle(3, 7, "purple")
+
+# Printing the results
+print(obj1)
+print(f"Perimeter: {obj1.perimeter()}, Area: {obj1.area()}\n")
+
+print(obj2)
+print(f"Perimeter: {obj2.perimeter()}, Area: {obj2.area()}\n")
+
+print(obj3)
+print(f"Perimeter: {obj3.perimeter()}, Area: {obj3.area()}\n")
