aeb58cd2c61c8c4bc047486f1032d641051e59fd
Mishozavri <mikheilabuashvili11@gmail.com>
2025-02-16 23:33:14 +0400

    dvaleba20

---
+++ davaleba20.py
@@ -0,0 +1,68 @@
+#1
+class People:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def get_email(self):
+        return f"{self.firstname}, {self.lastname}.mshobeli@gmail.com"
+
+    class Lecturer(people):
+        def __init__(self, firstname, lastname, salay):
+            super().__init__(firstname, lastname)
+
+            self.salay = salay
+
+    def get_email(self):
+        return f"{self.firstname}, {self.lastname}, {self.salay}.@svili1@gmail.com"
+
+    class Student(people):
+        def __init__(self, firstname, lastname, courses):
+            super().__init__(firstname, lastname)
+
+            self.courses = courses
+
+        def get_email(self):
+            return f"{self.firstname}, {self.lastname}, {self.courses}.shvili2@gmail.com"
+
+lecturer = Lecturer("misho", "abu", 5000)
+student = Student("gio", "abu", ["Math", "Physics"])
+
+print(lecturer.get_email())
+print(student.get_email())
+
+#2
+class LibraryItem:
+    def __init__(self, title, status="available"):
+        self.title = title
+        self.status = status
+
+    def booking(self):
+        if self.status = "available":
+            self.status = "occupied"
+            return f"{self.title} has been booked."
+        return f"{self.title} is already occupied."
+
+class Book(LibraryItem):
+    def __init__(self, title, authors, ISBN, status="available"):
+        super().__init__(title, status)
+        self.authors = authors
+        self.ISBN = ISBN
+
+class Magazine(LibraryItem):
+    def __init__(self, title, journalName, volume, status="available"):
+        super().__init__(title, status)
+        self.journalName = journalName
+        self.volume = volume
+
+class CD(LibraryItem):
+    def __init__(self, title, status="available"):
+        super().__init__(title, status)
+
+book = Book("Python junior", "Author Name", "123456")
+cd = CD("classic Music")
+
+print(book.booking())
+print(cd.booking())
+
+
