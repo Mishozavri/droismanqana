9260dd5a60002c7b1807735f34f84b37c5734492
Mishozavri <mikheilabuashvili11@gmail.com>
2025-06-09 19:38:37 +0400

    დავალება 36

---
+++ app2.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+from database import read, init_db, create, delete, update
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    data = read()
+    return render_template("main.html", todos=data)
+
+@app.route("/add", methods=["POST"])
+def add_todo():
+    todo_title = request.form["todo_title"]
+    todo_text = request.form["todo_text"]
+    create(todo_title, todo_text)
+    return redirect(url_for("main"))
+
+@app.route("/update", methods=["POST"])
+def update_todo():
+    todo_id = int(request.form["todo_id"])
+    new_title = request.form["todo_title"]
+    new_text = request.form["todo_text"]
+    update(todo_id, new_title, new_text)
+    return redirect(url_for("main"))
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
